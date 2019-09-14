ce039e6eadaaa0b9b0919e93295eb39e50c30c74
Samir <sam271091@gmail.com>
2019-09-15 00:19:31 +0400

    Rest api

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -10,10 +10,11 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    private static final String surname = "Test";
+    //private static final String surname = "Test";
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name,
+                             @RequestParam(value="surname", defaultValue="Test") String surname) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name),surname);
     }
